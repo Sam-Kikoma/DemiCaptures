93a7ffe7e4864053a55c5b42483b7348051f1851
Sam-Kikoma <kikomasamson@gmail.com>
2024-02-18 19:15:23 +0100

    Updated About component

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,6 +1,6 @@
 const About = () => {
 	return (
-		<div className="w-full text-black lg:grid lg:grid-cols-2">
+		<div className="w-full text-white lg:grid lg:grid-cols-2">
 			<article className="p-16">
 				<h1 className="text-center text-4xl md:text-6xl font-extrabold tracking-wider underline ">ABOUT</h1>
 				<p className="text-2xl my-16 text-justify leading-loose">
@@ -10,11 +10,11 @@ const About = () => {
 					together.
 				</p>
 			</article>
-			<article>
+			<article className="p-8">
 				<img
 					src="https://images.pexels.com/photos/1239291/pexels-photo-1239291.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
 					alt=""
-					className="max-w-full h-auto lg:h-full object-cover"
+					className="max-w-full h-auto lg:h-full object-cover rounded-xl"
 				/>
 			</article>
 		</div>
